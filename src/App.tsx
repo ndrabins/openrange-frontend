2e59f46ff82dcabdbd3f070e31be25a6f7fc76c2
Noah Drabinsky <noah.drabinsky1@gmail.com>
2019-07-15 19:01:25 -0600

    initial try at typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Button variant="contained">Button</Button>
     </div>
   );
-}
+};
 
 export default App;
