09fb7021e6c6466aff6e3aeb98dcc0693097287d
Noah Drabinsky <noah.drabinsky1@gmail.com>
2019-08-01 18:46:03 -0600

    remove font awesome added menu icon to navbar

--- src/commonComponents/Navbar.tsx
+++ src/commonComponents/Navbar.tsx
@@ -3,6 +3,7 @@ import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { DesignSystem } from "../utils/DesignSystem";
+import { MenuRounded } from "@material-ui/icons/";
 
 const NavContainer = styled.div`
   background-color: ${DesignSystem.colors.gray[800]};
@@ -10,16 +11,25 @@ const NavContainer = styled.div`
   display: flex;
   flex-direction: row;
   padding: ${DesignSystem.spacing[2]};
+  padding-left: ${DesignSystem.spacing[4]};
+  padding-right: ${DesignSystem.spacing[4]};
   align-items: center;
   justify-content: space-between;
   box-shadow: ${DesignSystem.shadow[2]};
 `;
 
+const StyledIcon = styled(MenuRounded)`
+  font-size: 50px;
+  color: ${DesignSystem.colors.cyan[500]};
+  width: 34px;
+  height: 34px;
+`;
+
 const NavBar: React.FC = () => {
   return (
     <NavContainer>
-      <div> something</div>
-      <div> blah</div>
+      <StyledIcon />
+      <div> profile</div>
     </NavContainer>
   );
 };
